7d9e15441a390fd885ad4d742fe8f42df16ab67a
Michael Stokoe <mstokoe0990@gmail.com>
2021-01-08 16:37:42 +0000

    add first name + last name prefills to JS

---
+++ material/login/resources/js/reg-filler.js
@@ -0,0 +1,36 @@
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let emailField = document.querySelector("input#email");
+    if (emailField) {
+        let emailAddress = getQueryVariable("email_address");
+        if (emailAddress){
+            field.value = emailAddress;
+        }
+    }
+
+    let firstNameField = document.querySelector("input#firstName");
+    if (firstNameField) {
+        let firstName = getQueryVariable("first_name");
+        if (firstName){
+            field.value = firstName;
+        }
+    }
+
+    let lastNameField = document.querySelector("input#lastName");
+    if (lastNameField) {
+        let lastName = getQueryVariable("last_name");
+        if (lastName){
+            field.value = lastName;
+        }
+    }
+});
