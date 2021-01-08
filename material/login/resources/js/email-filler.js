a4f4a3357246df18007c04a2e038e84ea66b8fda
Michael Stokoe <mstokoe0990@gmail.com>
2021-01-08 16:25:16 +0000

    add some JS for pre-filling email address

---
+++ material/login/resources/js/email-filler.js
@@ -0,0 +1,20 @@
+function getQueryVariable(variable)
+{
+    var query = window.location.search.substring(1);
+    var vars = query.split("&");
+    for (var i=0;i<vars.length;i++) {
+        var pair = vars[i].split("=");
+        if(pair[0] == variable){return pair[1];}
+    }
+    return(false);
+}
+
+document.addEventListener("DOMContentLoaded", function(event) { 
+    let field = document.querySelector("input#email");
+    if (field) {
+        let emailAddress = getQueryVariable("email-address");
+        if (emailAddress){
+            field.value = emailAddress;
+        }
+    }
+});
